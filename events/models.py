25d5105ccf5154ff37d8b499edbaf306b706a3fe
Oleksandr <aav162807@gmail.com>
2025-02-06 11:35:02 +0200

    Add Location model with name, address and description fields

--- events/models.py
+++ events/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField(max_length=255, null=True, blank=True)
+    description = models.TextField(max_length=500, null=True, blank=True)
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return self.name
