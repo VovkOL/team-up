88b09c1d75642fb34f04d74fe4a3aa7aeed58af1
Oleksandr <aav162807@gmail.com>
2025-02-06 12:32:36 +0200

    Add Athlete model with location, friends and custom methods

--- events/models.py
+++ events/models.py
@@ -23,3 +23,17 @@ class SportType(models.Model):
 
     def __str__(self):
         return self.name
+
+class Athlete(AbstractUser):
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)
+    friends = models.ManyToManyFields("self", symmetrical=True, blank=True)
+
+    class Meta:
+        verbose_name = "athlete"
+        verbose_name_plural = "athletes"
+
+    def __str__(self):
+        return f"{self.username}: ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("events:athlete-detail", kwargs={"pk": self.pk})
