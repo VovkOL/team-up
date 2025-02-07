89efeaf52ac043dccab8289d88a538fb7c14162b
Oleksandr <aav162807@gmail.com>
2025-02-07 11:52:04 +0200

    Add TrainingSession model with validations and relationships

--- events/models.py
+++ events/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.urls import reverse
 
@@ -26,7 +28,7 @@ class SportType(models.Model):
 
 class Athlete(AbstractUser):
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)
-    friends = models.ManyToManyFields("self", symmetrical=True, blank=True)
+    friends = models.ManyToManyField("self", symmetrical=True, blank=True)
 
     class Meta:
         verbose_name = "athlete"
@@ -37,3 +39,28 @@ class Athlete(AbstractUser):
 
     def get_absolute_url(self):
         return reverse("events:athlete-detail", kwargs={"pk": self.pk})
+
+
+class TrainingSession(models.Model):
+    sport_type = models.ForeignKey(SportType, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    host = models.ForeignKey(Athlete, on_delete=models.CASCADE, related_name="hosted_sessions")
+    athletes = models.ManyToManyField(Athlete,related_name="joined_sessions", blank=True)
+    max_athletes = models.PositiveIntegerField(
+        default=2,
+        validators=[
+            MinValueValidator(2, message="Max athletes cannot be less than 2."),
+            MaxValueValidator(50, message="Max athletes cannot be more than 50."),
+        ]
+    )
+    description = models.TextField(max_length=500, null=True, blank=True)
+
+    class Meta:
+        ordering = ("date_time", )
+
+    def __str__(self):
+        return f"{self.sport_type.name} on {self.date_time.strftime('%Y-%m-%d %H:%M')}"
+
+    def available_slots(self):
+        return self.max_athletes - self.athletes.count()
