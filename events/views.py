9b8f0a5d460038de280207c7b0d8632e18a76f0f
Oleksandr <aav162807@gmail.com>
2025-02-12 18:03:53 +0200

    Create index view with visit counter

--- events/views.py
+++ events/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from events.models import Location, SportType, Athlete, TrainingSession
+
+
+@login_required
+def index(request):
+    num_locations = Location.objects.count()
+    num_sport_types = SportType.objects.count()
+    num_athletes = Athlete.objects.count()
+    num_training_sessions = TrainingSession.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_locations": num_locations,
+        "num_sport_types": num_sport_types,
+        "num_athletes": num_athletes,
+        "num_training_sessions": num_training_sessions,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "events/index.html", conyext=context)
