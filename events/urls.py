184306406b4dd95649397cfa27ec209d8b7cb22c
Oleksandr <aav162807@gmail.com>
2025-03-04 11:15:32 +0200

    Add URL pattern for LocationDetailView

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     index,
-    LocationListView,
+    LocationListView, LocationDetailView,
 )
 
 
@@ -10,4 +10,5 @@ app_name = "events"
 urlpatterns = [
     path("", index, name="index"),
     path("locations/", LocationListView.as_view(), name="location-list"),
+    path("locations/<int:pk>/", LocationDetailView.as_view(), name="location-detail"),
 ]
