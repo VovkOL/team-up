b816c42024e603366e059df71045d482b4a084ca
Oleksandr <aav162807@gmail.com>
2025-03-08 20:38:55 +0200

    Add search form for SportType

--- events/forms.py
+++ events/forms.py
@@ -8,3 +8,12 @@ class LocationSearchForm(forms.Form):
         label="",
         widget=forms.TextInput(attrs={"placeholder": "Search by city name"}),
     )
+
+
+class SportTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by sport type"}),
+    )
