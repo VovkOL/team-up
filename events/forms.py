cc3c1e97af37dc88f878577027a041b8ffc71efd
Oleksandr <aav162807@gmail.com>
2025-03-12 16:02:06 +0200

    Refactor athlete forms: unique locations, validation, and cleanup

--- events/forms.py
+++ events/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from events.models import Athlete, Location
 
@@ -22,22 +23,59 @@ class SportTypeSearchForm(forms.Form):
     )
 
 
-class AthleteCreationForm(UserCreationForm):
+class LocationFilterMixin:
+    def get_filtered_locations(self):
+        unique_locations = {}
+        for location in Location.objects.all().order_by("name", "id"):
+            if location.name not in unique_locations:
+                unique_locations[location.name] = location.pk
+
+        return Location.objects.filter(pk__in=unique_locations.values()).order_by("name")
+
+
+class AthleteCreationForm(UserCreationForm, LocationFilterMixin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["location"].queryset = self.get_filtered_locations()
+
     location = forms.ModelChoiceField(
-        queryset=Location.objects.values_list("name", flat=True).distinct(),
+        queryset=Location.objects.all(),
         required=False,
         empty_label="Select a location",
     )
 
-    class Meta(UserCreationForm.Meta):
+    class Meta:
         model = Athlete
-        fields = (
-            "username",
-            "first_name",
-            "last_name",
-            "location",
-            "friends",
-            "password1",
-            "password2",
-        )
+        fields = ("username", "first_name", "last_name", "location", "password1", "password2")
 
+
+class AthleteSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=100,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"}),
+    )
+
+
+class AthleteUpdateForm(forms.ModelForm, LocationFilterMixin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["location"].queryset = self.get_filtered_locations()
+
+    location = forms.ModelChoiceField(
+        queryset=Location.objects.all(),
+        required=False,
+        empty_label="Select a location",
+    )
+
+
+    class Meta:
+        model = Athlete
+        fields = ["username", "first_name", "last_name", "location"]
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        if Athlete.objects.exclude(pk=self.instance.pk).filter(username=username).exists():
+            raise forms.ValidationError("This username is already taken.")
+        return username
